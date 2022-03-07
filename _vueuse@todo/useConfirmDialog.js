fa6f20c7755803ad7d1d0ff3768c0ca0caa2442c
luo0412 <1095847440@qq.com>
2022-03-07 15:55:24 +0800

    adjust

---
+++ _vueuse@todo/useConfirmDialog.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+
+const App = {
+  template: `
+
+<div>
+    
+</div>
+
+  `,
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
